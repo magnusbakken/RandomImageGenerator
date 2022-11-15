df8a476db9a0362075e5d0f736924a760746f964
Magnus Grindal Bakken <magnusbakken@gmail.com>
2022-11-15 11:26:36 +0100

    Added logging to DeepAIGenerator.

--- ImageGeneration/DeepAIGenerator.cs
+++ ImageGeneration/DeepAIGenerator.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net.Http.Json;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace RandomImageGenerator.ImageGeneration;
@@ -8,30 +9,38 @@ public class DeepAIGenerator : IImageGenerator
     private const string Url = "https://api.deepai.org/api/text2img";
     private readonly DeepAIOptions _options;
     private readonly HttpClient _httpClient;
+    private readonly ILogger<DeepAIGenerator> _logger;
 
-    public DeepAIGenerator(IHttpClientFactory httpClientFactory, IOptions<DeepAIOptions> options)
+    public DeepAIGenerator(IHttpClientFactory httpClientFactory, IOptions<DeepAIOptions> options, ILogger<DeepAIGenerator> log)
     {
         _options = options.Value;
         _httpClient = httpClientFactory.CreateClient(nameof(DeepAIGenerator));
         _httpClient.DefaultRequestHeaders.Add("api-key", _options.ApiKey);
+        _logger = log;
     }
 
     public async Task<byte[]> Generate(string sentence, CancellationToken cancellationToken)
     {
+        _logger.LogInformation("Generating image for sentence: {0}", sentence);
         var formContent = new FormUrlEncodedContent(new[] { new KeyValuePair<string, string>("text", sentence) });
         var response = await _httpClient.PostAsync(Url, formContent, cancellationToken);
         if (response.IsSuccessStatusCode)
         {
+            _logger.LogInformation("Successfully generated image for sentence: {0}", sentence);
             var content = await response.Content.ReadFromJsonAsync<DeepAIResponse>(cancellationToken: cancellationToken);
             if (content != null)
+            {
+                _logger.LogInformation("Downloading image from {0}", content.OutputUrl);
                 return await _httpClient.GetByteArrayAsync(content.OutputUrl, cancellationToken);
+            }
         }
         else
         {
             var content = await response.Content.ReadAsStringAsync(cancellationToken);
-            Console.WriteLine(content);
+            _logger.LogInformation("Got unsuccessful response from DeepAI: [{0}]: {1}", response.StatusCode, content);
         }
 
+        _logger.LogInformation("Failed to generate image for sentence: {0}", sentence);
         return Array.Empty<byte>();
     }
 }
