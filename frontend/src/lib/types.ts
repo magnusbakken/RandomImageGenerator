bea3d562c9a4ffbbdf9780c0f256552aa9b6c9a1
Magnus Grindal Bakken <magnusbakken@gmail.com>
2023-10-25 00:07:02 +0200

    PoC

---
+++ frontend/src/lib/types.ts
@@ -0,0 +1,4 @@
+export interface ImageData {
+    link: string;
+    sentence: string;
+}
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,37 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import type { ImageData } from '$lib/types';
+	import Preloader from '$lib/components/preloader.svelte';
+
+	let loading = false;
+	let imageData: ImageData | null = null;
+
+	async function generate(): Promise<void> {
+		loading = true;
+		imageData = null;
+		const url = 'https://randomimagegenerator.azurewebsites.net/api/GenerateImageLink';
+		const request = JSON.stringify({ textgen: 'OpenAI', imagegen: 'OpenAI' });
+		const result = await fetch(url, { method: 'POST', body: request });
+		const body = await result.json();
+		imageData = body;
+		loading = false;
+	}
+</script>
+
+<div>
+	<button on:click={generate} disabled={loading}>Generate</button>
+</div>
+
+{#if loading}
+	<div>
+		<Preloader />
+	</div>
+{/if}
+
+{#if imageData !== null}
+	<div>
+		<figure>
+			<img src={imageData.link} alt="AI-generated" />
+			<figcaption>{imageData.sentence}</figcaption>
+		</figure>
+	</div>
+{/if}
